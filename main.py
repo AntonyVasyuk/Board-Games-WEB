32ee61f7bbbb06462dfcb4caf2f448126285f574
vasyu <vasyuk.a.a@yandex.ru>
2024-04-23 18:45:28 +0300

    Changes for server

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import os
+
 import flask
-import flask_login
-import werkzeug
-from flask import Flask, render_template
-from flask_login import LoginManager, AnonymousUserMixin
+from flask import Flask, session
+from flask_login import LoginManager
 from functools import wraps
-import flask_login
 from flask import render_template, redirect
-from flask_login import login_user
 
 from classes.login_form import LoginForm
 from classes.register_form import RegisterForm
@@ -24,23 +22,48 @@ games = [Game(i) for i in range(1, 4)]
 refresh_time = 1000
 
 
+def current_user():
+    data = session.get("current_user", None)
+    user = None
+    if (data is not None):
+        user = User(
+                name=data["name"],
+                about=data["about"],
+                email=data["email"]
+            )
+    return user
+
+
+def login_user(user):
+    data = {
+        "name": user.name,
+        "about": user.about,
+        "email": user.email
+    }
+    session["current_user"] = data
+
+
+def logout_user():
+    session["current_user"] = None
+
+
 def is_cur_user_in_game():
     g = None
     for game in games:
-        if (flask_login.current_user in game.players):
+        if (current_user() in game.players):
             g = game
             break
     return g
 
 
 def render_template_new(*args, **kwargs):
-    return render_template(*args, **kwargs, user=flask_login.current_user)
+    return render_template(*args, **kwargs, user=current_user())
 
 
 def authorised_only(f):
     @wraps(f)
     def wrapper_f(*args, **kwargs):
-        if (not flask_login.current_user.is_authenticated):
+        if (current_user() is None):
             return render_template_new("401.html", title="Error 401")
         else:
             return f(*args, **kwargs)
@@ -71,7 +94,7 @@ def login():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
+            login_user(user)
             return redirect("/user")
         return render_template_new('login.html',
                                message="Неправильный логин или пароль",
@@ -96,7 +119,7 @@ def register():
         user = User(
             name=form.username.data,
             about=form.about.data,
-            email=form.email.data,
+            email=form.email.data
         )
         user.set_password(form.password.data)
         db_sess = db_session.create_session()
@@ -120,7 +143,7 @@ def index():
 
 @app.route("/")
 @authorised_only
-# @redirect_if_playing
+@redirect_if_playing
 def main_page():
     return render_template_new("main.html", title="Main page")
 
@@ -157,7 +180,7 @@ def game(key):
         keys = [g.join_code for g in games]
         if (key in keys):
             g = games[keys.index(key)]
-            if (not g.player_join(flask_login.current_user)):
+            if (not g.player_join(current_user())):
                 return redirect("/find_game")
             # print(g.players[0].email)
             # return "2"
@@ -172,7 +195,7 @@ def game(key):
 @authorised_only
 # @redirect_if_playing
 def logout():
-    flask_login.logout_user()
+    logout_user()
     return redirect("./")
 
 
@@ -181,7 +204,9 @@ def logout():
 
 def main():
     db_session.global_init("db/tictactoe.db")
-    app.run(host="127.0.0.1", port=8000)
+    # session["current_user"] = None
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="127.0.0.1", port=port)
 
 
 if __name__ == "__main__":
